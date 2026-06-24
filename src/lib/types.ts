7420f0c78db26d802544bf384ac20692445bb22d
Delta <salmonzik01@gmail.com>
2026-06-24 21:31:09 +0200

    fix: change zone.id to required because generated in data.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,35 +1,35 @@
 export interface Room {
-  id: string;
-  name: string;
-  x: number;
-  y: number;
-  w: number;
-  h: number;
-  zones: Zone[];
+	id: string;
+	name: string;
+	x: number;
+	y: number;
+	w: number;
+	h: number;
+	zones: Zone[];
 }
 
 export enum ZoneType {
-  CABINET = "cabinet",
-  FUMEHOOD = "fumehood",
-  FREEZER = "freezer",
-  BENCH = "bench",
-  SINK = "sink",
-  RACK = "rack",
+	CABINET = 'cabinet',
+	FUMEHOOD = 'fumehood',
+	FREEZER = 'freezer',
+	BENCH = 'bench',
+	SINK = 'sink',
+	RACK = 'rack',
 }
 
 export interface Zone {
-  id?: string;
-  name: string;
-  type: ZoneType;
-  x: number;
-  y: number;
-  w: number;
-  h: number;
-  shelves?: Shelf[];
-  sublabel?: string;
+	id: string;
+	name: string;
+	type: ZoneType;
+	x: number;
+	y: number;
+	w: number;
+	h: number;
+	shelves?: Shelf[];
+	sublabel?: string;
 }
 
 export interface Shelf {
-  name?: string;
-  items: string[];
+	name?: string;
+	items: string[];
 }
